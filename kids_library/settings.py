dce1ee68f1e809d9697706a7a2d11c56dcee4fd2
code1xXx <mickytod2255@gmial.com>
2023-09-17 17:32:14 -0700

    second commit

--- kids_library/settings.py
+++ kids_library/settings.py
@@ -151,4 +151,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mickytod03@gmail.com'
-EMAIL_HOST_PASSWORD = 'velyvmhmarllevni'
+EMAIL_HOST_PASSWORD = 'qaua ypis ltph cnaa'
+
+
